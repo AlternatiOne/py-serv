101500e468fd51bb6001f8adcc988321cd990d5d
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2019-11-20 17:19:34 +0300

    Clear .gitignore file and format code

--- server.py
+++ server.py
@@ -6,18 +6,18 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(1)
 while True:
-   conn, addr = s.accept()
-   while True:
-     data = conn.recv(1024)
-   # print(data)
-     if not data:
+    conn, addr = s.accept()
+    while True:
+        data = conn.recv(1024)
+        # print(data)
+        if not data:
+            break
+        if data == "close":
+            conn.close()
+            break
+        else:
+            conn.send(data)
+    if data == "close":
         break
-     if data == "close": 
+    else:
         conn.close()
-        break
-     else:
-        conn.send(data)
-   if data == "close": 
-      break
-   else:
-     conn.close()
